2b03f3f7c60564537dd694b074e7e03bf12286c6
NogyJ0D <valentingiarra2@gmail.com>
2024-03-10 20:36:26 -0300

    first commit

---
+++ src/main/java/ar/com/vgiarra/dcm4vgviewer/Dcm4vgViewerApplication.java
@@ -0,0 +1,13 @@
+package ar.com.vgiarra.dcm4vgviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dcm4vgViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dcm4vgViewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/vgiarra/dcm4vgviewer/Dcm4vgViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.vgiarra.dcm4vgviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dcm4vgViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
